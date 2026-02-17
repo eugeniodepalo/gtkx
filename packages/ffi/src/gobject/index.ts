24d1351417d97bde6e31fd0a6d7fad25c7d37d62
Eugenio Depalo <eugeniodepalo@gmail.com>
2026-02-17 19:18:08 +0000

    Add manual FFI binding for g_object_new_with_properties

--- packages/ffi/src/gobject/index.ts
+++ packages/ffi/src/gobject/index.ts
@@ -1,3 +1,4 @@
 export * from "../generated/gobject/index.js";
+export * from "./object.js";
 export * from "./types.js";
 export * from "./value.js";
---
+++ packages/ffi/src/gobject/object.ts
@@ -0,0 +1,80 @@
+import type { NativeHandle } from "@gtkx/native";
+import { Object as GObject } from "../generated/gobject/object.js";
+import type { Value } from "../generated/gobject/value.js";
+import { call } from "../native.js";
+import { getNativeObject } from "../registry.js";
+
+declare module "../generated/gobject/object.js" {
+    namespace Object {
+        /**
+         * Creates a new instance of a GObject subtype and sets its properties
+         * using the provided arrays. Both arrays must have exactly the same
+         * number of elements, and the names and values correspond by index.
+         *
+         * Construction parameters (see G_PARAM_CONSTRUCT, G_PARAM_CONSTRUCT_ONLY)
+         * which are not explicitly specified are set to their default values.
+         *
+         * @param objectType - The GType of the object to instantiate
+         * @param names - The names of each property to be set
+         * @param values - The values of each property to be set
+         * @returns A new instance of the specified type
+         */
+        // biome-ignore lint/complexity/noBannedTypes: Refers to GObject.Object class, not global Object
+        function newWithProperties(objectType: number, names: string[], values: Value[]): Object;
+    }
+}
+
+const LIB = "libgobject-2.0.so.0";
+const GVALUE_SIZE = 24;
+
+type ObjectStatic = {
+    newWithProperties(objectType: number, names: string[], values: Value[]): GObject;
+};
+
+const ObjectWithStatics = GObject as typeof GObject & ObjectStatic;
+
+ObjectWithStatics.newWithProperties = (objectType: number, names: string[], values: Value[]): GObject => {
+    const ptr = call(
+        LIB,
+        "g_object_new_with_properties",
+        [
+            {
+                type: { type: "int", size: 64, unsigned: true },
+                value: objectType,
+            },
+            {
+                type: { type: "int", size: 32, unsigned: true },
+                value: names.length,
+            },
+            {
+                type: {
+                    type: "array",
+                    itemType: { type: "string", ownership: "borrowed" },
+                    kind: "sized",
+                    sizeParamIndex: 1,
+                    ownership: "borrowed",
+                },
+                value: names,
+            },
+            {
+                type: {
+                    type: "array",
+                    itemType: {
+                        type: "boxed",
+                        ownership: "borrowed",
+                        innerType: "GValue",
+                        library: LIB,
+                        getTypeFn: "g_value_get_type",
+                    },
+                    kind: "sized",
+                    sizeParamIndex: 1,
+                    elementSize: GVALUE_SIZE,
+                    ownership: "borrowed",
+                },
+                value: values.map((v) => v.handle),
+            },
+        ],
+        { type: "gobject", ownership: "full" },
+    );
+    return getNativeObject(ptr as NativeHandle) as GObject;
+};
