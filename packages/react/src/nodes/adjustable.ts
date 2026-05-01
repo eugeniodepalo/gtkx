bd8dd13dd15ba0b40a1aeb12c82f353c0d4f5b4c
Eugenio Depalo <eugeniodepalo@gmail.com>
2026-05-01 18:12:20 +0100

    Extract AdjustmentController from Adjustable and SpinRow

--- packages/react/src/nodes/adjustable.ts
+++ packages/react/src/nodes/adjustable.ts
@@ -1,39 +1,26 @@
-import * as Gtk from "@gtkx/ffi/gtk";
+import type * as Gtk from "@gtkx/ffi/gtk";
 import type { AdjustableProps } from "../jsx.js";
 import type { AdjustableWidget } from "../registry.js";
+import { ADJUSTMENT_PROPS, AdjustmentController } from "./internal/adjustment.js";
 import { filterProps, hasChanged } from "./internal/props.js";
 import { WidgetNode } from "./widget.js";
 
-const OWN_PROPS = ["value", "lower", "upper", "stepIncrement", "pageIncrement", "pageSize", "onValueChanged"] as const;
-
 export class AdjustableNode<T extends AdjustableWidget = AdjustableWidget> extends WidgetNode<T, AdjustableProps> {
-    private adjustment: Gtk.Adjustment | null = null;
+    private adjustmentController = new AdjustmentController(this.container);
 
     public override commitUpdate(oldProps: AdjustableProps | null, newProps: AdjustableProps): void {
-        super.commitUpdate(oldProps ? filterProps(oldProps, OWN_PROPS) : null, filterProps(newProps, OWN_PROPS));
+        super.commitUpdate(
+            oldProps ? filterProps(oldProps, ADJUSTMENT_PROPS) : null,
+            filterProps(newProps, ADJUSTMENT_PROPS),
+        );
         this.applyOwnProps(oldProps, newProps);
     }
 
     protected ensureAdjustment(props: AdjustableProps): Gtk.Adjustment {
-        if (!this.adjustment) {
-            this.adjustment = new Gtk.Adjustment(
-                props.value ?? 0,
-                props.lower ?? 0,
-                props.upper ?? 100,
-                props.stepIncrement ?? 1,
-                props.pageIncrement ?? 10,
-                props.pageSize ?? 0,
-            );
-
-            this.container.setAdjustment(this.adjustment);
-        }
-
-        return this.adjustment;
+        return this.adjustmentController.apply(null, props);
     }
 
     protected applyOwnProps(oldProps: AdjustableProps | null, newProps: AdjustableProps): void {
-        const adjustment = this.ensureAdjustment(newProps);
-
         if (hasChanged(oldProps, newProps, "onValueChanged")) {
             const { onValueChanged } = newProps;
             this.signalStore.set(
@@ -44,25 +31,6 @@ export class AdjustableNode<T extends AdjustableWidget = AdjustableWidget> exten
             );
         }
 
-        if (!oldProps) return;
-
-        if (hasChanged(oldProps, newProps, "lower")) {
-            adjustment.setLower(newProps.lower ?? 0);
-        }
-        if (hasChanged(oldProps, newProps, "upper")) {
-            adjustment.setUpper(newProps.upper ?? 100);
-        }
-        if (hasChanged(oldProps, newProps, "stepIncrement")) {
-            adjustment.setStepIncrement(newProps.stepIncrement ?? 1);
-        }
-        if (hasChanged(oldProps, newProps, "pageIncrement")) {
-            adjustment.setPageIncrement(newProps.pageIncrement ?? 10);
-        }
-        if (hasChanged(oldProps, newProps, "pageSize")) {
-            adjustment.setPageSize(newProps.pageSize ?? 0);
-        }
-        if (hasChanged(oldProps, newProps, "value") && newProps.value !== undefined) {
-            adjustment.setValue(newProps.value);
-        }
+        this.adjustmentController.apply(oldProps, newProps);
     }
 }
---
+++ packages/react/src/nodes/internal/adjustment.ts
@@ -0,0 +1,47 @@
+import * as Gtk from "@gtkx/ffi/gtk";
+import type { AdjustableProps } from "../../jsx.js";
+import { hasChanged } from "./props.js";
+
+export const ADJUSTMENT_PROPS = [
+    "value",
+    "lower",
+    "upper",
+    "stepIncrement",
+    "pageIncrement",
+    "pageSize",
+    "onValueChanged",
+] as const;
+
+export class AdjustmentController {
+    private adjustment: Gtk.Adjustment | null = null;
+
+    constructor(private container: { setAdjustment: (a: Gtk.Adjustment) => void }) {}
+
+    apply(oldProps: AdjustableProps | null, newProps: AdjustableProps): Gtk.Adjustment {
+        if (!this.adjustment) {
+            this.adjustment = new Gtk.Adjustment(
+                newProps.value ?? 0,
+                newProps.lower ?? 0,
+                newProps.upper ?? 100,
+                newProps.stepIncrement ?? 1,
+                newProps.pageIncrement ?? 10,
+                newProps.pageSize ?? 0,
+            );
+            this.container.setAdjustment(this.adjustment);
+            return this.adjustment;
+        }
+
+        if (!oldProps) return this.adjustment;
+
+        const adjustment = this.adjustment;
+        if (hasChanged(oldProps, newProps, "lower")) adjustment.setLower(newProps.lower ?? 0);
+        if (hasChanged(oldProps, newProps, "upper")) adjustment.setUpper(newProps.upper ?? 100);
+        if (hasChanged(oldProps, newProps, "stepIncrement")) adjustment.setStepIncrement(newProps.stepIncrement ?? 1);
+        if (hasChanged(oldProps, newProps, "pageIncrement")) adjustment.setPageIncrement(newProps.pageIncrement ?? 10);
+        if (hasChanged(oldProps, newProps, "pageSize")) adjustment.setPageSize(newProps.pageSize ?? 0);
+        if (hasChanged(oldProps, newProps, "value") && newProps.value !== undefined) {
+            adjustment.setValue(newProps.value);
+        }
+        return adjustment;
+    }
+}
