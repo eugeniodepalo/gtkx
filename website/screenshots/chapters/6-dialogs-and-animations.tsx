05e33fa21c0fedd9bb7af193c4fc8b2d7d383ff9
Eugenio Depalo <eugeniodepalo@gmail.com>
2026-05-20 21:44:03 +0200

    Share NotesSplitShell between chapter 5 and 6 screenshots

--- website/screenshots/chapters/6-dialogs-and-animations.tsx
+++ website/screenshots/chapters/6-dialogs-and-animations.tsx
@@ -1,55 +1,15 @@
-import * as Gtk from "@gtkx/ffi/gtk";
-import {
-    AdwApplicationWindow,
-    AdwHeaderBar,
-    AdwNavigationSplitView,
-    AdwToggleGroup,
-    AdwToolbarView,
-    GtkListView,
-    GtkMenuButton,
-    GtkScrolledWindow,
-} from "@gtkx/react";
-import { type Note, NoteCardComponent, NotesSidebarPage, noop, sampleNotes } from "../data";
+import { AdwToggleGroup } from "@gtkx/react";
+import { NotesSplitShell } from "../NotesSplitShell";
 
 export const Chapter6 = () => (
-    <AdwApplicationWindow title="Notes" defaultWidth={900} defaultHeight={600} onClose={noop}>
-        <AdwNavigationSplitView sidebarWidthFraction={0.25} minSidebarWidth={200} maxSidebarWidth={300}>
-            <NotesSidebarPage />
-            <AdwNavigationSplitView.Page id="content" title="All Notes">
-                <AdwToolbarView>
-                    <AdwToolbarView.AddTopBar>
-                        <AdwHeaderBar>
-                            <AdwHeaderBar.PackEnd>
-                                <AdwToggleGroup
-                                    toggles={[
-                                        { id: "list", iconName: "view-list-symbolic" },
-                                        { id: "grid", iconName: "view-grid-symbolic" },
-                                    ]}
-                                />
-                            </AdwHeaderBar.PackEnd>
-                            <AdwHeaderBar.PackEnd>
-                                <GtkMenuButton iconName="open-menu-symbolic">
-                                    <GtkMenuButton.MenuItem id="about" label="About Notes" onActivate={noop} />
-                                    <GtkMenuButton.MenuItem
-                                        id="quit"
-                                        label="Quit"
-                                        accels="<Control>q"
-                                        onActivate={noop}
-                                    />
-                                </GtkMenuButton>
-                            </AdwHeaderBar.PackEnd>
-                        </AdwHeaderBar>
-                    </AdwToolbarView.AddTopBar>
-                    <GtkScrolledWindow vexpand>
-                        <GtkListView
-                            estimatedItemHeight={80}
-                            selectionMode={Gtk.SelectionMode.SINGLE}
-                            items={sampleNotes.map((n) => ({ id: n.id, value: n }))}
-                            renderItem={(note: Note) => <NoteCardComponent note={note} />}
-                        />
-                    </GtkScrolledWindow>
-                </AdwToolbarView>
-            </AdwNavigationSplitView.Page>
-        </AdwNavigationSplitView>
-    </AdwApplicationWindow>
+    <NotesSplitShell
+        headerEndExtras={
+            <AdwToggleGroup
+                toggles={[
+                    { id: "list", iconName: "view-list-symbolic" },
+                    { id: "grid", iconName: "view-grid-symbolic" },
+                ]}
+            />
+        }
+    />
 );
